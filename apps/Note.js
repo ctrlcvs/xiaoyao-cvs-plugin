5e974ce463ae6cb193cf20f0d3622095b9598a92
leiyilu <1509167646@qq.com>
2022-06-25 10:02:49 +0800

    修改体力最快显示异常

--- apps/Note.js
+++ apps/Note.js
@@ -111,13 +111,16 @@ export async function Note(e, {
 		}
 	}
 	for (let val of data.expeditions) {
-		val.remained_time = new Date().getTime() + val.remained_time * 1000;
-		let remainedDate = new Date(val.remained_time);
-		val.remained_time = format("hh:mm", remainedDate);
-		if (format("dd", remainedDate) != nowDay) {
-			val.remained_time = `明天 ${val.remained_time}`;
-		} else {
-			val.remained_time = ` ${val.remained_time}`;
+		// console.log(val.remained_time)
+		if (val.remained_time > 0) {
+			val.remained_time = new Date().getTime() + val.remained_time * 1000;
+			let remainedDate = new Date(val.remained_time);
+			val.remained_time = format("hh:mm", remainedDate);
+			if (format("dd", remainedDate) != nowDay) {
+				val.remained_time = `明天 ${val.remained_time}`;
+			} else {
+				val.remained_time = ` ${val.remained_time}`;
+			}
 		}
 	}
 	let remained_time = "";
@@ -178,7 +181,7 @@ export async function Note(e, {
 	for (let val of image) {
 		list_img.push(val)
 	}
-	var imgs =list_img.length==1?list_img[0]:list_img[lodash.random(0, list_img.length - 1)];
+	var imgs = list_img.length == 1 ? list_img[0] : list_img[lodash.random(0, list_img.length - 1)];
 	return await Common.render("dailyNote/dailyNote", {
 		save_id: uid,
 		uid: uid,
