227b5c331afa4f48e738c98afbbf17d1c725c714
leiyilu <1509167646@qq.com>
2022-06-28 23:48:04 +0800

    ..

--- apps/Note.js
+++ apps/Note.js
@@ -137,6 +137,7 @@ export async function Note(e, {
 				time_cha = 12;
 			}
 			val.percentage = ((val.dq_time / 60 / 60 * 1 / time_cha) * 100 / 10).toFixed(0) * 10;
+			if(val.dq_time==0) val.percentage=0;
 			let remainedDate = new Date(val.remained_time);
 			val.remained_time = format("hh:mm", remainedDate);
 			let Time_day = await dateTime_(remainedDate)
