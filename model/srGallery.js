5cdbda77a2f4d3d4120503f920a9e2c0f00678c2
ctrlcvs <1509167646@qq.com>
2023-06-07 18:49:29 +0800

    优化武器别名匹配，增加`*银狼四星专武`匹配

--- model/srGallery.js
+++ model/srGallery.js
@@ -22,25 +22,38 @@ export async function AtlasAlias(e,{render}) {
 
 
 export async function GetData(e) {
-    let name=e.msg.replace(/\*|#|星铁|星穹铁道|图鉴|专武/g,'')
+    let name=e.msg.replace(/\*|#|星铁|(四|4)星|(五|5)星|星穹铁道|图鉴|专武/g,'')
     let list = Data.readJSON(pathPlus, 'weapon/data.json')
     let items = Data.readJSON(pathPlus, 'items/data.json')
     let role=GetRole(name)
+    let isUp=false;
     if(role) {
         name=role.name
+        isUp=true
     }
-    let roleData;
-    let isUp=e.msg.includes('专武');
+    let roleData,roleList =[];
     lodash.forEach(list,(v,k)=>{
         if(isUp&&v.belongRole.includes(name)){
-            roleData= v
+            roleList.push(v)
             return
         }else if([v.name,...v.names,...v?.suitRole].includes(name)&&!isUp){
-            roleData= v
+            roleList.push(v)
             return
         }
     })
-
+    let isUp4=/4|四/.test(e.msg)
+    let isUp5=/5|五/.test(e.msg)
+    if(roleList.length==0) return false
+    roleData=roleList[0]
+    for (const role of roleList) {
+        if(isUp4&&role.star==4){
+            roleData=role
+        }else if (isUp5&&role.star==5){
+            roleData=role
+        }else if(!isUp4&&!isUp5&&roleData.star<role.star){
+            roleData=role
+        }
+    }
     if(roleData){
         let newMaterial=[]
         for (const materialElement of roleData.material) {
