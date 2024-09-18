cdec9f01aa9e3f96264f7c7820424fa1404a21d9
ctrlcvs <1509167646@qq.com>
2024-09-18 08:47:02 +0800

    图库默认gitee链接改为gitea（感谢@小灰灰）

--- apps/admin.js
+++ apps/admin.js
@@ -174,9 +174,10 @@ export async function updateRes(e) {
 			}
 		});
 	} else {
-		//gitee图床
-		command = `git clone https://github.com/ctrlcvs/xiaoyao_plus.git "${resPath}/xiaoyao-plus/"`
-		// command = `git clone https://gitee.com/Ctrlcvs/xiaoyao-plus.git "${resPath}/xiaoyao-plus/"`;\n此链接为github图床,如异常请请求多次
+		//gitea图床
+		command = `git clone http://gitea.microgg.cn/ctrlcvs/xiaoyao_plus.git "${resPath}/xiaoyao-plus/"`
+		// command = `git clone https://gitee.com/Ctrlcvs/xiaoyao-plus.git "${resPath}/xiaoyao-plus/"`;\n此链接为gitee图床,因仓库过大已被封禁
+		// command = `git clone https://github.com/ctrlcvs/xiaoyao_plus.git "${resPath}/xiaoyao-plus/"`;\n此链接为gitub图床,如异常请请求多次
 		e.reply("开始尝试安装图鉴加量包，可能会需要一段时间，请耐心等待~");
 		exec(command, function(error, stdout, stderr) {
 			if (error) {
